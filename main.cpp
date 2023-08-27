2674f4b574e9fb382615f4855ce8997bdb474a57
chizk0 <126604899+chizk0@users.noreply.github.com>
2023-08-27 02:39:49 -0600

    [add commit]
    Co-Authored-By: JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -3,27 +3,82 @@
 #include <cstdlib>
 #include <fstream>
 #include <string>
-
+#include <stack>
+#include <algorithm>
+#include <cmath>
 using namespace std;
+
+class Variable
+{
+private:
+    char nombre;
+    bool valores[16];
+    bool noValores[16];
+    int contFilas;
+
+public:
+    Variable()
+    {
+        this->nombre = ' ';
+        for (int i = 0; i < 16; i++)
+        {
+            this->valores[i] = false;
+            noValores[i] = false;
+        }
+    }
+    Variable(char nombre)
+    {
+        this->nombre = nombre;
+        for (int i = 0; i < 16; i++)
+        {
+            this->valores[i] = false;
+        }
+    }
+    void setValor(bool valor)
+    {
+        this->valores[contFilas] = valor;
+        this->noValores[contFilas] = !valor;
+        contFilas++;
+    }
+    bool getValor(int fila)
+    {
+        return this->valores[fila];
+    }
+    bool getNoValor(int fila)
+    {
+        return this->noValores[fila];
+    }
+    void setNombre(char nombre)
+    {
+        this->nombre = nombre;
+    }
+};
+
+string contarVariables(string);
+void imprimirTabla();
 string abrirTxt();
 void leer();
+void tablaVerdad();
+void generarTablaVerdad();
+void separarExpresiones(string);
+int maxFila = 0;
+int maxColumna = 0;
+stack<string> pila;
+string archivo;
+string variablesStr = " ";
+Variable arrVariables[5];
+
 int main(int argc, char const *argv[])
 {
+
     int respuesta = 0;
-    string archivo;
-    string archi;
-    char variables[5];
-    for (int i = 0; i < 5; ++i) {
-        variables[i] = '\0';  
-    }
     do
     {
         respuesta = -1;
         cout << "-----------------------------------------------------" << endl;
         cout << "Menu'" << endl;
         cout << " 1 Digitar documento" << endl;
-        cout << " 2 Ver mapas de karnaugh " << endl;
-        cout << " 3 Ver simplificacion " << endl;
+        cout << " 2 Ver tablas" << endl;
         cout << " 0 finalizar" << endl;
         cin >> respuesta;
         cout << "-----------------------------------------------------" << endl;
@@ -34,14 +89,20 @@ int main(int argc, char const *argv[])
         case 1:
             archivo = abrirTxt();
             cout << archivo << endl;
+            variablesStr = contarVariables(archivo);
+            cout << archivo << endl;
             break;
         case 2:
+            tablaVerdad();
+            imprimirTabla();
+            // separarExpresiones(archivo);
+            // generarTablaVerdad();
             cout << "Primero debe ingresar un documento" << endl;
             break;
         case 3:
             break;
         case 0:
-            break; // este caso es para que no de el mensaje de default porque 0 si es una obcion
+            break;
         default:
             cout << "Su respuestas no esta entre las opciones " << endl;
         }
@@ -74,10 +135,115 @@ string abrirTxt()
     while (!arc.eof())
     {
         getline(arc, txt);
-		
     }
-    cout << txt << endl;
     arc.close();
 
     return txt;
+}
+string contarVariables(const string txt)
+{
+    string stringVariables = "";
+    for (char c : txt)
+    {
+        if (isalpha(c))
+        {
+            if (stringVariables.find(c) == string::npos)
+            {
+                stringVariables += c;
+            }
+        }
+    }
+
+    sort(stringVariables.begin(), stringVariables.end()); // abc
+    for (int i = 0; i < stringVariables.length(); i++)
+    {
+        arrVariables[i].setNombre(stringVariables[i]);
+    }
+    return stringVariables;
+}
+
+void tablaVerdad()
+{
+    maxFila = pow(2, variablesStr.length());
+    maxColumna = variablesStr.length();
+  
+
+    for (int i = 0; i < maxFila; i++)
+    {
+        for (int j = maxColumna - 1; j >= 0; j--)
+        {
+            if (((i / (int)pow(2, j)) % 2) == 1)
+            {
+                arrVariables[maxColumna - j].setValor(true);
+            }
+            else
+            {
+                arrVariables[maxColumna - j].setValor(false);
+            }
+        }
+        cout << endl;
+    }
+}
+void imprimirTabla()
+{
+    for (int i = 0; i < maxFila; i++)
+    {
+        for (int j = 0; j < maxColumna; j++)
+        {
+            if (arrVariables[j].getValor(j))
+            {
+                cout << "1"
+                     << " ";
+            }
+            else
+            {
+                cout << "0"
+                     << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+void separarExpresiones(string archivo)
+{
+    string expresion = "";
+    for (char c : archivo)
+    {
+        if (c == ',')
+        {
+            pila.push(expresion);
+            expresion = "";
+        }
+        else
+        {
+            expresion += c;
+        }
+    }
+    pila.push(expresion);
+    cout << pila.top() << endl;
+}
+void generarTablaVerdad()
+{ /*
+     stack<string> pilaAux = pila;
+     for (int i = 0; i < maxColumna; i++)
+     {
+         cout << "|" << variables[i] << "|";
+     }
+     for (int p = 0; p < pila.size(); p++)
+     {
+         cout << pilaAux.top() << endl;
+         pilaAux.pop();
+     }
+     cout << endl;
+     for (int i = 0; i < maxFila; i++)
+     {
+         for (int j = 0; j < maxColumna; j++)
+         {
+             cout << table[i][j] << " ";
+         }
+         cout << endl;
+
+         // operacion
+     }
+     */
 }
